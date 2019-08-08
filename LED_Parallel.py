f5fe00964db62442d2177db184cb10aeba9b1368
cnourshargh <camron.nourshargh@worc.ox.ac.uk>
2019-08-08 12:03:30 +0100

    First commit of LED: Parallel code Renamed LED: On, Off, Pulse code

---
+++ LED_Parallel.py
@@ -0,0 +1,30 @@
+from artiq.experiment import*                   #imports everything from artiq experiment library
+
+#example code for using with parallel and with sequential
+
+class tutorial_LEDrealTime(EnvExperiment):
+    """LED Parallel"""
+    def build(self): #Adds the device drivers as attributes and adds the keys to the kernel invarients
+    
+        self.setattr_device("core")             #required in all builds   
+        self.setattr_device("led0")             #required for using led0
+        self.setattr_device("led1")             #required for using led1
+
+        
+        
+    @kernel #this code is run on the FPGA
+    def run(self):
+    
+        self.core.reset()                       #resets core device
+        
+        self.core.break_realtime()              #moves timestamp forward to prevent underflow
+                                                #this can also be achieved with a fixed delay
+
+        with parallel:                          #instructs core device to run contained code in parallel
+        
+            self.led0.pulse(4*s)                #led0 turns on for 5s then turns off
+            
+            with sequential:                    #instructs core device to run contained code sequentially 
+            
+                self.led1.pulse(2*s)            #led1 turns on for 2s then turns off
+                delay(2*s)                      #2s delay
