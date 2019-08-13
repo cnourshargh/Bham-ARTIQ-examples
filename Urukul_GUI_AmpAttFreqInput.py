b5900d579796fd310684a0d8b20b8082adeca4b4
cnourshargh <camron.nourshargh@worc.ox.ac.uk>
2019-08-13 15:43:09 +0100

    improved comments

--- Urukul_GUI_AmpAttFreqInput.py
+++ Urukul_GUI_AmpAttFreqInput.py
@@ -1,8 +1,11 @@
 from artiq.experiment import*                                   #imports everything from the artiq experiment library
 
 #This code outputs a single frequency at a fixed amplitude on a single channel of the urukul
-#the output frequency must be inputted in MHz from the dahsboard
-#The ouput persists for 2 seconds and the turns off
+#The following must be input from the dashboard:
+#frequency(in MHz)
+#amplitude(as amplitude scale factor so between 0 and 1)
+#attenuation(in db, between 0 and 31.5)
+#pulse length(in s)
 
 class tutorial_urukulFreqScan(EnvExperiment):
     """Urukul Selectable Frequency, Amplitude, Attenuation and Pulse Length"""
@@ -11,10 +14,10 @@ class tutorial_urukulFreqScan(EnvExperiment):
 
         self.setattr_device("core")                                                     #sets core device drivers as attributes
         self.setattr_device("urukul0_ch1")                                              #sets urukul0, channel 1 device drivers as attributes
-        self.setattr_argument("freq", NumberValue(ndecimals=0, unit="MHz", step=1))     #instructs dashboard to take input and set it as an attribute called freq
-        self.setattr_argument("amp", NumberValue(ndecimals=2, step=1))                  #instructs dashboard to take input and set it as an attribute called freq
-        self.setattr_argument("atten", NumberValue(ndecimals=2, step=1, scale=1))                #instructs dashboard to take input and set it as an attribute called freq
-        self.setattr_argument("t_pulse", NumberValue(ndecimals=0, unit = "s", step=1))  #instructs dashboard to take input and set it as an attribute called freq
+        self.setattr_argument("freq", NumberValue(ndecimals=0, unit="MHz", step=1))     #instructs dashboard to take input in MHz and set it as an attribute called freq
+        self.setattr_argument("amp", NumberValue(ndecimals=2, step=1))                  #instructs dashboard to take input and set it as an attribute called amp
+        self.setattr_argument("atten", NumberValue(ndecimals=2, step=1))                #instructs dashboard to take input and set it as an attribute called atten
+        self.setattr_argument("t_pulse", NumberValue(ndecimals=2, unit = "s", step=1))  #instructs dashboard to take input and set it as an attribute called t_pulse
         
     
     @kernel #This code runs on the FPGA
@@ -24,13 +27,11 @@ class tutorial_urukulFreqScan(EnvExperiment):
         self.urukul0_ch1.init()                                 #initialises channel 1
         delay(10 * ms)                                          #10ms delay
         
-        #amp = 1.0                                               #defines amplitude variable as an amplitude scale factor(0 to 1)
-        #attenuation= 1.0                                        #defines attenuation variable
         
-        self.urukul0_ch1.set_att(self.atten)                   #writes attenuation to urukul channel
+        self.urukul0_ch1.set_att(self.atten)                    #writes attenuation to urukul channel
         self.urukul0_ch1.sw.on()                                #switches urukul channel on
            
             
-        self.urukul0_ch1.set(self.freq, amplitude = self.amp)  #writes frequency and amplitude variables to urukul channel thus outputting function
-        delay(self.t_pulse * s)                                              #2s delay
+        self.urukul0_ch1.set(self.freq, amplitude = self.amp)   #writes frequency and amplitude attributes to urukul channel thus outputting function
+        delay(self.t_pulse * s)                                 #delay determined by user input
         self.urukul0_ch1.sw.off()                               #switches urukul channel off
